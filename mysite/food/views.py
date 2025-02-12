63d81cad3526af1e1e573b8dcee18c22207b28ef
sanjip mahat <146052754+sanjip1203@users.noreply.github.com>
2025-02-12 16:53:04 +0545

    make food apps and connect with data and render data from database connect with admin

---
+++ mysite/food/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+# Create your views here.
+def index(request):
+    item_list =Item.objects.all()
+    context ={
+        'item_list':item_list,
+        
+    }
+    return render(request,'food/index.html',context)
+
+
+def item(request):
+    return HttpResponse ("this is items ")
