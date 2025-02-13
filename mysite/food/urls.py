c1600bd7015528bd7f0f242ddb406e48a6dac1d9
sanjip mahat <146052754+sanjip1203@users.noreply.github.com>
2025-02-13 21:41:42 +0545

    add curd operation in food app with sqlite

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -3,8 +3,15 @@ from django.urls import path
 
 app_name ='food'
 urlpatterns = [
+    ## food 
     path('',views.index,name='index'),
+    ## /food/1
     path('<int:item_id>/',views.detail,name='detail'),
     path('item/',views.item,name='item'),
-    
+    ## add items 
+    path('add',views.create_item,name='create_item'),
+    ## edit 
+    path('update/<int:id>',views.update_item,name="update_item"),
+    ## delete 
+    path('delete/<int:id>',views.delete_item,name="delete_item"),
 ]
